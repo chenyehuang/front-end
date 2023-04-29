71295883ee13173e2f2ac83aeb53a2cd2e0632eb
chenrenxing <Crx672501109@163.com>
2023-04-29 20:26:26 +0800

    完成了“我的收藏”界面

--- miniprogram-1/pages/mine/mine.js
+++ miniprogram-1/pages/mine/mine.js
@@ -2,14 +2,12 @@
 Page({
   data: {
     userinfo:{},
-    // 被收藏的商品的数量
-    collectNums:0
   },
-  // onShow(){
-  //   const userinfo=wx.getStorageSync("userinfo");
-  //   const collect=wx.getStorageSync("collect")||[];
+  onShow(){
+    const userinfo=wx.getStorageSync("userinfo");
+    const collect=wx.getStorageSync("collect")||[];
       
-  //   this.setData({userinfo,collectNums:collect.length});
+    this.setData({userinfo,collectNums:collect.length});
       
-  // }
+  }
 })
